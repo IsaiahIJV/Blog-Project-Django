b36174fe9cfa0f056d96ff39efb5273a08edfb19
Isaiah Vertus <isaiah.vertus@gmail.com>
2018-12-10 20:02:57 -0500

    Pushing Blog Application Project

---
+++ blog/views.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.views.generic import ListView, DetailView
+
+from . models import Post
+
+
+class BlogListView(ListView):
+
+	model = Post
+
+	template_name = 'home.html'
+
+
+
+class BlogDetailView(DetailView):
+
+	model = Post
+
+	template_name = 'post_detail.html'
